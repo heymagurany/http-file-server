88ba602b3defc62ce5a184f3f7dacf71ae3688c4
Matt Magurany <mmagurany@live.com>
2017-10-25 11:30:54 -0400

    Add usage message

--- server.go
+++ server.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
+	args := os.Args[1:]
+	if len(args) < 2 {
+		fmt.Printf("usage: http-file-server <port> <file name>\n")
+		os.Exit(1)
+	}
+	port, fileName := args[0], args[1]
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		http.ServeFile(w, req, os.Args[2])
+		http.ServeFile(w, req, fileName)
 	})
-	log.Fatal(http.ListenAndServe(":"+os.Args[1], nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
