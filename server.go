983866b2a749c1ba56fc3847309d015d9b29f083
Matt Magurany <mmagurany@live.com>
2018-01-11 18:00:19 -0500

    Add option for URL path

--- server.go
+++ server.go
@@ -10,11 +10,17 @@ import (
 func main() {
 	args := os.Args[1:]
 	if len(args) < 2 {
-		fmt.Printf("usage: http-file-server <port> <file name>\n")
+		fmt.Printf("usage: http-file-server <port> <file name> [url path]\n")
 		os.Exit(1)
 	}
 	port, fileName := args[0], args[1]
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+	var path string
+	if len(args) > 2 {
+		path = args[3]
+	} else {
+		path = "/"
+	}
+	http.HandleFunc(path, func(w http.ResponseWriter, req *http.Request) {
 		fmt.Printf("%s %s\n", req.Method, req.URL.String())
 		http.ServeFile(w, req, fileName)
 	})
